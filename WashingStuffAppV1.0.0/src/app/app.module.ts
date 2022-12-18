8468a95b23b1c7ec238c9f7b86662f519c4bc1a5
JorgeR <jlramoss@yahoo.com.mx>
2022-12-18 04:57:45 -0600

    initialized Angular application with all modules and components needed
    Phonix-0001

--- WashingStuffAppV1.0.0/src/app/app.module.ts
+++ WashingStuffAppV1.0.0/src/app/app.module.ts
@@ -2,12 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NavBarComponentComponent } from './nav-bar-component/nav-bar-component.component';
 
+const routes: Routes = [
+  { path: 'home', component: HomeComponentComponent },
+  
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponentComponent,
+    NavBarComponentComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule
   ],
   providers: [],
